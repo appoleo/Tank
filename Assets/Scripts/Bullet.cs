186fd7b8cbbbadb2cf2b55ddbed0d0ca4e50df16
wangchendong <appoleo@163.com>
2021-01-20 14:00:55 +0800

    bullet public method

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
+    // 子弹移动速度
     public float moveSpeed = 10;
+    // 是否是玩家的子弹
+    public bool isPlayerBullet;
 
     // Start is called before the first frame update
     void Start()
@@ -25,19 +28,25 @@ public class Bullet : MonoBehaviour
         switch (collision.tag)
         {
             case "Tank":
-                collision.SendMessage("Die");
+                if (!isPlayerBullet)
+                {
+                    // 调用坦克死亡方法
+                    collision.SendMessage("Die");
+                }
                 break;
             case "Heart":
-
+                // 调用心脏死亡方法
+                collision.SendMessage("Die");
                 break;
             case "Enemy":
 
                 break;
             case "Wall":
-
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
                 break;
             case "Barrier":
-
+                Destroy(gameObject);
                 break;
             default:
                 break;
